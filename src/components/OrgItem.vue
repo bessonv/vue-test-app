ec26359ab7bcc26538c3f46caccfd413f6182441
denis <denis@edu.yar.ru>
2022-08-05 13:21:47 +0300

    phone number fix

--- src/components/OrgItem.vue
+++ src/components/OrgItem.vue
@@ -32,8 +32,8 @@ export default defineComponent({
       this.itemId = this.organization.id
       this.organizationName = this.organization.orgName
       this.directorName = this.organization.dirName
-      const mobile = this.organization.phone.match(/(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})/)
-      const home = this.organization.phone.match(/(\d{2})(\d{2})(\d{2})/)
+      const mobile = this.organization.phone.match(/^(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})$/)
+      const home = this.organization.phone.match(/^(\d{2})(\d{2})(\d{2})$/)
       const phone = mobile || home
       if (phone) {
         phone.shift()
--- src/mockData.ts
+++ src/mockData.ts
@@ -4,15 +4,15 @@ const data: orgType[] = [
   { id: 1, orgName: 'ООО "Вектор"', dirName: 'Иванов И.И.', phone: '70001234567' },
   { id: 2, orgName: 'ИП Сидоров С.С.', dirName: 'Сидоров С.С.', phone: '70000567899' },
   { id: 3, orgName: 'ООО "Дреуг"', dirName: 'Калинин Я.И.', phone: '75851514461' },
-  { id: 4, orgName: 'ООО "Лпиов"', dirName: 'Белозёров В.А.', phone: '722600111547' },
-  { id: 5, orgName: 'ООО "Бцедс"', dirName: 'Ситников А.С.', phone: '7651806765' },
-  { id: 6, orgName: 'ООО "Нкуиш"', dirName: 'Жуков А.Х.', phone: '717355467170' },
-  { id: 7, orgName: 'ООО "Гнини"', dirName: 'Тарасов Т.Д.', phone: '7914617263' },
+  { id: 4, orgName: 'ООО "Лпиов"', dirName: 'Белозёров В.А.', phone: '72260011154' },
+  { id: 5, orgName: 'ООО "Бцедс"', dirName: 'Ситников А.С.', phone: '77651806765' },
+  { id: 6, orgName: 'ООО "Нкуиш"', dirName: 'Жуков А.Х.', phone: '467170' },
+  { id: 7, orgName: 'ООО "Гнини"', dirName: 'Тарасов Т.Д.', phone: '77914617263' },
   { id: 8, orgName: 'ООО "Еяпюп"', dirName: 'Орлов А.А.', phone: '74410702448' },
-  { id: 9, orgName: 'ООО "Юмеип"', dirName: 'Фокин Д.А.', phone: '793471771805' },
+  { id: 9, orgName: 'ООО "Юмеип"', dirName: 'Фокин Д.А.', phone: '771805' },
   { id: 10, orgName: 'ООО "Лбигу"', dirName: 'Беспалов И.Н.', phone: '72377232352' },
-  { id: 11, orgName: 'ООО "Гмуфп"', dirName: 'Овчинников А.Э.', phone: '7640339677' },
-  { id: 12, orgName: 'ООО "Окэнэ"', dirName: 'Новиков В.М.', phone: '7940396810' }
+  { id: 11, orgName: 'ООО "Гмуфп"', dirName: 'Овчинников А.Э.', phone: '77640339677' },
+  { id: 12, orgName: 'ООО "Окэнэ"', dirName: 'Новиков В.М.', phone: '77940396810' }
 ]
 
 export default data
